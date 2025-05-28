691e4e0376af502d0d71a7b0d89dcaed91e25150
Kim Steinhaug <kim@steinhaug.com>
2025-05-28 06:34:22 +0200

    Codeblock was duplicated for some reason

--- chrome-extension/content-script.js
+++ chrome-extension/content-script.js
@@ -14,13 +14,6 @@ chrome.runtime.onMessage.addListener(({scriptType, scriptContents}) => {
         document.head.appendChild(style);
     }
 
-  } else if ( scriptType === "css" ){
-    const style = document.createElement('style');
-    style.type = 'text/css';
-    style.appendChild(document.createTextNode(scriptContents));
-    style.setAttribute("data-witchcraft", "");  // to make it easy finding the element if we want to
-    document.head.appendChild(style);
-  }
 });
 
 chrome.runtime.sendMessage(location);
